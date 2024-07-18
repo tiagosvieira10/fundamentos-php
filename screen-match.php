e49aec0999e2e4f46d203111ff30c008d3b59b43
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-07-18 12:17:09 -0300

    first commit

---
+++ screen-match.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Bem-vindo ao screen match!\n";
+
+$nomeFilme = "Top Gun - Marverick";
+$anoLancamento = $argv[1] ?? 2020;
+$notaFilme = 8.8;
+$incluidoNoPlano = true;
+
+echo "Nome do filme: $nomeFilme\n";
+echo "Ano de lançamento: $anoLancamento\n";
