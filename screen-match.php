4b3c78ac18e4697f64dcb2c76ee4a42c9586470d
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-07-18 13:32:58 -0300

    arrays php

--- screen-match.php
+++ screen-match.php
@@ -9,3 +9,11 @@ $incluidoNoPlano = true;
 
 echo "Nome do filme: $nomeFilme\n";
 echo "Ano de lançamento: $anoLancamento\n";
+
+if ($anoLancamento > 2022) {
+  echo "Esse filme é um lançamento!";
+} elseif($anoLancamento > 2020 && $anoLancamento <= 2022) {
+  echo "Esse filme ainda é novo\n";
+} else {
+  echo "Esse filme não é um lançamento\n";
+}
